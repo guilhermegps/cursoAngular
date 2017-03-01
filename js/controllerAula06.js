804439bec5b0c6bdfd7d626182585a9ff6b4b765
Guilherme de Paiva <gpsantos1@latam.stefanini.com>
2017-03-01 15:34:08 -0300

    Testes com controller

---
+++ js/controllerAula06.js
@@ -0,0 +1,9 @@
+app.controller('controllerAula06', function($scope){
+  $scope.nome = "Curso AngularJS";
+
+  $scope.olaMundo = function() {
+    alert('Hello World!');
+  };
+
+  console.log("Controle executado");
+});
